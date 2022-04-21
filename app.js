d9da821d118569dc199c3c9c69742b0eb6e05bd2
pranab99 <impranabdas99@gmail.com>
2022-04-22 01:18:43 +0530

    ssdfs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(morgan("dev"));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -31,6 +31,27 @@ app.get('/noninst', async(req,res)=>{
 })
 app.get('/adjreg', async(req,res)=>{
     res.render("AdjReg")
+})
+
+const InstReg=require('./models/InstReg')
+
+app.post('/instregPost',async(req,res)=>{
+    const NOI= req.body.NOI;
+    const NOTSR = req.body.NOTSR;
+    const NOIA=req.body.NOIA;
+    const NOIR = req.body.NOIR;
+    const CNOIR= req.body.CNOIR;
+    const EAOIR = req.body.EAOIR;
+
+    InstReg.create({
+    name_of_inst:              NOI,
+    number_of_team_slots_req:  NOTSR,
+    number_of_inst_adjud:      NOIA,
+    name_of_inst_rep:          NOIR,
+    contact_number_of_inst_rep:CNOIR,
+    email_of_inst_rep:         EAOIR 
+    })
+    // res.render("SubmissionSuccessful")   
 })
 
 
